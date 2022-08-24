8aca762a60dea7da72485c5db4454ca2d47f75bb
Samuel Omanchi <omanchis@gmail.com>
2022-08-24 09:35:44 +0200

    Temperarily removed google analytics

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,18 +1,18 @@
-import Head from 'next/head';
-import { ReactNode } from 'react-markdown';
-import styles from './layout.module.scss';
+import Head from "next/head";
+import { ReactNode } from "react-markdown";
+import styles from "./layout.module.scss";
 
-const name = 'Samuel Omanchi';
-export const siteTitle = 'Samuel Omanchi';
+const name = "Samuel Omanchi";
+export const siteTitle = "Samuel Omanchi";
 
 export const Layout: React.FC<{ children: ReactNode }> = ({ children }) => {
   return (
     <div className={styles.container}>
       <Head>
-        <link rel='icon' href='/favicon.ico' />
+        <link rel="icon" href="/favicon.ico" />
         <meta
-          name='Samuel Omanchi'
-          content='Front end web developement articles. Learn HTML, CSS, Javacript, React, React Native.'
+          name="Samuel Omanchi"
+          content="Front end web developement articles. Learn HTML, CSS, Javacript, React, React Native."
         />
         {/* <meta
           property='og:image'
@@ -20,10 +20,10 @@ export const Layout: React.FC<{ children: ReactNode }> = ({ children }) => {
             siteTitle
           )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
         /> */}
-        <meta name='og:title' content={siteTitle} />
-        <meta name='twitter:card' content='summary_large_image' />
+        <meta name="og:title" content={siteTitle} />
+        <meta name="twitter:card" content="summary_large_image" />
 
-        <script
+        {/* <script
           async
           src='https://www.googletagmanager.com/gtag/js?id=UA-134151606-2'
         ></script>
@@ -37,7 +37,7 @@ export const Layout: React.FC<{ children: ReactNode }> = ({ children }) => {
             
               gtag('config', 'UA-134151606-2');`,
           }}
-        />
+        /> */}
       </Head>
 
       <main>{children}</main>
