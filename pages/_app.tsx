651f173ea4fddf5e9068d941b6cddab8f60d3ad8
Samuel Omanchi <omanchis@gmail.com>
2022-12-09 23:22:44 +0100

    use head for scripts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,20 @@
 import "@scss/global.scss";
 import { Analytics } from "@vercel/analytics/react";
 import { NextScript } from "next/document";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }) {
   return (
     <>
-      <script
-        async
-        src={`https://www.googletagmanager.com/gtag/js?id=G-${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}`}
-      />
+      <Head>
+        <script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=G-${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}`}
+        />
 
-      <script
-        dangerouslySetInnerHTML={{
-          __html: `
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
@@ -20,11 +22,12 @@ export default function App({ Component, pageProps }) {
               page_path: window.location.pathname,
             });
                 `,
-        }}
-      />
+          }}
+        />
+      </Head>
+
       <Component {...pageProps} />
       <Analytics />
-      <NextScript />
     </>
   );
 }
