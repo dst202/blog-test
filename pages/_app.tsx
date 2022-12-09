e9e2d2c1d215caaec4fa543977d7e8469b972225
Samuel Omanchi <omanchis@gmail.com>
2022-12-09 17:48:36 +0100

    Add google analytics in app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,26 @@
 import SiteAnalyticsScript from "@lib/PageScript";
 import "@scss/global.scss";
 import { Analytics } from "@vercel/analytics/react";
+import Script from "next/script";
 
 export default function App({ Component, pageProps }) {
   return (
     <>
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}`}
+      />
+
+      <Script id="google-analytics" strategy="lazyOnload">
+        {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}', {
+              page_path: window.location.pathname,
+            });
+                `}
+      </Script>
       <Component {...pageProps} />
       <Analytics />
       <SiteAnalyticsScript />
