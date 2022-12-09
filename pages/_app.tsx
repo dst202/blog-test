c868361773c4c0868e9a02e0a8bc00bdf7cec847
Samuel Omanchi <omanchis@gmail.com>
2022-12-09 23:34:55 +0100

    Change script tag GA!!!!

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -10,18 +10,16 @@ export default function App({ Component, pageProps }) {
         <script
           async
           src={`https://www.googletagmanager.com/gtag/js?id=G-${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}`}
-        />
+        ></script>
 
         <script
+          async
           dangerouslySetInnerHTML={{
-            __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}', {
-              page_path: window.location.pathname,
-            });
-                `,
+            __html: `window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+            
+              gtag('config', 'G-${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}');`,
           }}
         />
       </Head>
