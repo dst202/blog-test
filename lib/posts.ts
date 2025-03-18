191525723343c1824f2945d86b5c9b0b03938c1c
Surya Chilukuri <suryatejach202@gmail.com>
2025-03-18 08:23:06 +0100

    updated posts.ts

--- lib/posts.ts
+++ lib/posts.ts
@@ -4,22 +4,17 @@ import path from 'path';
 import { cache } from 'react';
 import remark from 'remark';
 import html from 'remark-html';
+import remarkGfm from 'remark-gfm'; // Import the remark-gfm plugin
+
 const postsDirectory = path.join(process.cwd(), 'posts');
 
 export function getSortedPostsData() {
-  // Get file names under /posts
   const fileNames = fs.readdirSync(postsDirectory);
   const allPostsData = fileNames.map((fileName) => {
-    // Remove ".md" from file name to get id
     const id = fileName.replace(/\.md$/, '');
-
-    // Read markdown file as string
     const fullPath = path.join(postsDirectory, fileName);
     const fileContents = fs.readFileSync(fullPath, 'utf8');
-
-    // Use gray-matter to parse the post metadata section
     const matterResult = matter(fileContents);
-    // Combine the data with the id
     return {
       id,
       title: matterResult.data.title,
@@ -30,55 +25,30 @@ export function getSortedPostsData() {
       ...matterResult.data,
     };
   });
-  // Sort posts by date
-  return allPostsData.sort((a: any, b: any) => {
-    if (a.date < b.date) {
-      return 1;
-    } else {
-      return -1;
-    }
-  });
+  return allPostsData.sort((a: any, b: any) => (a.date < b.date ? 1 : -1));
 }
 
 export function getAllPostIds() {
   const fileNames = fs.readdirSync(postsDirectory);
-
-  // Returns an array that looks like this:
-  // [
-  //   {
-  //     params: {
-  //       id: 'ssg-ssr'
-  //     }
-  //   },
-  //   {
-  //     params: {
-  //       id: 'pre-rendering'
-  //     }
-  //   }
-  // ]
-  return fileNames.map((fileName) => {
-    return {
-      params: {
-        id: fileName.replace(/\.md$/, ''),
-      },
-    };
-  });
+  return fileNames.map((fileName) => ({
+    params: {
+      id: fileName.replace(/\.md$/, ''),
+    },
+  }));
 }
 
 export const getPostData = cache(async (id: string) => {
   const fullPath = path.join(postsDirectory, `${id}.md`);
   const fileContents = fs.readFileSync(fullPath, 'utf8');
-
-  // Use gray-matter to parse the post metadata section
   const matterResult = matter(fileContents);
 
-  // Use remark to convert markdown into HTML string
+  // Use remark to convert markdown into HTML string with GFM support
   const processedContent = await remark()
+    .use(remarkGfm) // Enable GitHub Flavored Markdown features, including tables
     .use(html)
     .process(matterResult.content);
   const contentHtml = processedContent.toString();
 
-  // Combine the data with the id and contentHtml
   return {
     id,
     contentHtml,
